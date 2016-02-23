a6c18a36f1ec09a05627d32e8d0d038af90629e8
Carlos Abdalla <abdalla@msdev.com.br>
2016-02-22 22:29:44 -0300

    almost working

--- server.js
+++ server.js
@@ -1,12 +1,45 @@
 "use strict";
 
-var express = require('express');
-var app = express();
+let express = require('express');
+let path = require('path');
 
-app.get('/', function(req, res){
-  res.send(`Teu Pet`);
+let webpack = require('webpack');
+let webpackDevServer = require('webpack-dev-server');
+let config = require('./webpack.config');
+
+let app = express();
+
+app.set('port', process.env.PORT || 8080);
+app.use(express.static(path.join(__dirname, 'src')));
+
+app.get('*', function(req, res){
+  res.sendFile(path.resolve(__dirname, 'src', 'index.html'));
 })
 
-var server = app.listen(3000, function(){
-  console.log(`Server is running at http://localhost:${server.address().port}`);
-})
+const webpackPort = parseInt(app.get('port'),10) + 1;
+
+if(process.env.environment != "prod") {
+  new webpackDevServer(webpack(config), {
+     historyApiFallback: false,
+     stats: {
+       colors: true
+     },
+     proxy: {
+       "*": "http://localhost:" + app.get('port')
+     }
+  }).listen(webpackPort, '0.0.0.0', function (err, result) {
+     if (err) {
+       console.log(err);
+     }
+     console.log(`Listening at localhost:${webpackPort}`);
+  });
+}
+
+/**
+ * Start Express server.
+ */
+app.listen(app.get('port'), function() {
+  console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
+});
+
+module.exports = app;
---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+require('babel-polyfill');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Teu Pet</h1>,
+  document.getElementById('react')
+);
