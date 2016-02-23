7faf6ac52bba70756575958b81d260b5aee56af5
Carlos Abdalla <abdalla@msdev.com.br>
2016-02-23 18:51:25 -0300

    all working, missing react and redux.. but the configuration is good

--- src/index.js
+++ src/index.js
@@ -13,13 +13,21 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const server = app.listen(3000, function () {
+  console.log('Express listening on port 3000');
+});
+
+//sample routes
 //import dogs from './routes/dogs';
 //app.use('/dogs', dogs);
 
-//app.get('/hello', (req, res) => {
-//  res.send('Hello Earth!');
-//});
+app.get('/hello', (req, res) => {
+  res.send('Hello Carlos, my master!');
+});
 
-const server = app.listen(3000, function () {
-  console.log('Express listening on port 3000');
+app.get('/add/:x/:y', (req, res) => {
+  const x = req.params.x * 1;
+  const y = req.params.y * 1;
+
+  res.send({sum: x+y});
 });
