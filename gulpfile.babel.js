1f5b580214740437cbf4669246cc8d84a3513996
Carlos Abdalla <abdalla@msdev.com.br>
2016-02-24 20:05:08 -0300

    trying to compress the js files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,12 +4,15 @@ import rimraf from 'rimraf';
 import run from 'run-sequence';
 import watch from 'gulp-watch';
 import server from 'gulp-live-server';
+import uglify from 'gulp-uglify';
+import concat from 'gulp-concat';
 
 
 const paths = {
   js: ['./src/**/*.js'],
+  source: 'src',
   destination: './app',
-  serverPath: './server.js'
+  destinationServer: './server.js'
 };
 
 gulp.task('default', callback => {
@@ -17,6 +20,7 @@ gulp.task('default', callback => {
 });
 
 gulp.task('build', callback => {
+  //run('clean:app', 'clean:server', 'flow', 'babel', 'compress', 'restart', callback);
   run('clean:app', 'clean:server', 'flow', 'babel', 'restart', callback);
 });
 
@@ -25,22 +29,30 @@ gulp.task('clean:app', callback => {
 });
 
 gulp.task('clean:server', callback => {
-  rimraf(paths.serverPath , callback);
+  rimraf(paths.destinationServer , callback);
 });
 
 gulp.task('flow', shell.task([
   'flow'
 ], {ignoreErrors: true}));
 
+//gulp.task('compress', ['babel'], () => {
+//  return gulp
+//      .src('./app/**/*.js')
+//      .pipe(concat('bundle.js'))
+//      .pipe(uglify())
+//      .pipe(gulp.dest('./app/min/'));
+//});
+
 gulp.task('babel', shell.task([
-  'babel src/js --out-dir app',
-  'babel src/server --out-dir .'
+  `babel ${paths.source}/js --out-dir app`,
+  `babel ${paths.source}/server --out-dir .`
 ]));
 
 let express;
 
 gulp.task('server', () => {
-    express = server.new(paths.serverPath );
+    express = server.new(paths.destinationServer);
 });
 
 gulp.task('restart', () => {
